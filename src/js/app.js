a1bbfebcdf29ec78f032a521d483d61e647af98f
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2023-10-04 18:06:38 +0600

    2d/3d switcher

--- src/js/app.js
+++ src/js/app.js
@@ -2,7 +2,8 @@ const showStage = document.getElementById('show-stage');
 const listStages = document.querySelector('.list-stages');
 const firstStage = document.getElementById('first-stage')
     secondStage = document.getElementById('second-stage')
-    allStage = document.getElementById('all-stage');
+    allStage = document.getElementById('all-stage')
+    toggleD = document.getElementById('toggleD');
 
 function showStageList() {
     const checkClass = listStages.classList;
@@ -12,25 +13,128 @@ function showStageList() {
 
 showStage.addEventListener('click', showStageList);
 
-firstStage.addEventListener('click', () => {
-    let visibility = map.getLayoutProperty('room-extrusion', 'visibility');
-    // const features = map.queryRenderedFeatures({ layers: ['room-extrusion'] });
-    // console.log(features)
-    // features.forEach(element => {
-    //     element.properties.level === 2 
-    //         ? map.setLayoutProperty(element.id, 'visibility', 'none') 
-    //         : map.setLayoutProperty(element.id, 'visibility', 'visible') 
-    //     console.log(element.layer)
-    // });
-    visibility === 'visible' 
-        ? map.setLayoutProperty('room-extrusion', 'visibility', 'none') 
-        : map.setLayoutProperty('room-extrusion', 'visibility', 'visible');
-})
+function changeStageVis(polygonId, pointId) {
+    let visibility = map.getLayoutProperty(polygonId, 'visibility');
 
-secondStage.addEventListener('click', () => {
-    let visibility = map.getLayoutProperty('room-extrusion2', 'visibility');
+    if (visibility === 'visible' ) {
+        map.setLayoutProperty(polygonId, 'visibility', 'none');
+        // map.setLayoutProperty(pointId, 'visibility', 'none');
+    } else {
+        map.setLayoutProperty(polygonId, 'visibility', 'visible');
+        // map.setLayoutProperty(pointId, 'visibility', 'visible');
+    }
+}
 
-    visibility === 'visible' 
-        ? map.setLayoutProperty('room-extrusion2', 'visibility', 'none') 
-        : map.setLayoutProperty('room-extrusion2', 'visibility', 'visible');
-})
+firstStage.addEventListener('click', () => changeStageVis('room-extrusion', 'point_lvl1'));
+secondStage.addEventListener('click', () => changeStageVis('room-extrusion2', 'point_lvl2'));
+
+allStage.addEventListener('click', () => {
+    changeStageVis('room-extrusion', 'point_lvl1');
+    changeStageVis('room-extrusion2', 'point_lvl2');
+});
+
+toggleD.addEventListener('click', changeDimension);
+
+function changeDimension() {
+    if (toggleD.innerHTML === '2D') {
+        toggleD.innerHTML = '3D';
+        map.setMaxPitch(0);
+        map.setBearing(0)
+    } else {
+        toggleD.innerHTML = '2D';
+        map.setMaxPitch(60);
+        map.setPitch(60);
+    }
+    changeLayerType();
+}
+
+function changeLayerType() {
+    const layerType = map.getLayer('room-extrusion').type;
+    if (layerType === 'fill-extrusion') {
+
+        map.removeLayer('room-extrusion');
+        map.removeLayer('room-extrusion2');
+
+        map.addLayer({
+            id: 'room-extrusion',
+            type: 'fill',
+            source: 'im',
+            paint: {
+                'fill-color': ['get', 'color'],
+            },
+            filter: ['==', ['get', 'level'], 1],
+        });
+
+        map.addLayer({
+            id: 'room-extrusion2',
+            type: 'fill',
+            source: 'im',
+            paint: {
+                'fill-color': ['get', 'color'],
+            },
+            filter: ['==', ['get', 'level'], 2],
+        }); 
+       
+        map.addLayer({
+            id: 'point_lvl1',
+            type: 'symbol',
+            source: 'im',
+            layout: {
+                visibility: 'visible',
+                'icon-image': 'room',
+                'icon-size': 0.06, // Adjust the size as needed
+            },
+            'filter': ['==', ['get', 'level'], 11]
+        });
+
+        map.addLayer({
+            id: 'point_lvl2',
+            type: 'circle',
+            source: 'im',
+            layout: {
+                visibility: 'visible',
+            },
+            'paint': {
+                'circle-radius': 6,
+                'circle-color': '#B42222',
+                },
+            'filter': ['==', ['get', 'level'], 12]
+        });
+    } else {
+        map.removeLayer('room-extrusion');
+        map.removeLayer('room-extrusion2');
+        map.addLayer({
+            id: 'room-extrusion',
+            type: 'fill-extrusion',
+            source: 'im',
+            layout: {
+                visibility: 'visible',
+            },
+            paint: {
+                'fill-extrusion-color': ['get', 'color'],
+                'fill-extrusion-height': ['get', 'height'],
+                'fill-extrusion-base': ['get', 'base_height'],
+                'fill-extrusion-opacity': 1
+            },
+            filter: ['==', ['get', 'level'], 1]
+        });
+    
+        map.addLayer({
+            id: 'room-extrusion2',
+            type: 'fill-extrusion',
+            source: 'im',
+            layout: {
+                visibility: 'visible',
+            },
+            paint: {
+                'fill-extrusion-color': ['get', 'color'],
+                'fill-extrusion-height': ['get', 'height'],
+                'fill-extrusion-base': ['get', 'base_height'],
+                'fill-extrusion-opacity': 1
+            },
+            filter: ['==', ['get', 'level'], 2]
+        });
+
+    };
+ 
+};
--- src/js/map.js
+++ src/js/map.js
@@ -34,20 +34,35 @@ const map = (window.map = new maplibregl.Map({
     zoom: 19,
     center: [71.428572161323359, 51.091671321981359],
     pitch: 60,
+    bearing: 0,
     antialias: true
 }));
 
+const navControl = new maplibregl.NavigationControl({
+    visualizePitch: true,
+    showZoom: false,
+    showCompass: true,
+});
+
+map.addControl(navControl, 'top-left')
+
 map.on('load', () => {
+    map.loadImage('./src/ext/door.png', function(error, image) {
+        if (error) throw error;
+        // Add the loaded image to the style's sprite with the ID 'kitten'.
+        map.addImage('room', image);
+    });
     map.addSource('im', {
-        'type': 'geojson',
-        'data': './src/ext/2im_4326.geojson',
+        type: 'geojson',
+        data: './src/ext/2im_4326.geojson',
         generateId: true
     });
+
     
     map.addLayer({
-        'id': 'room-extrusion',
-        'type': 'fill-extrusion',
-        'source': 'im',
+        id: 'room-extrusion',
+        type: 'fill-extrusion',
+        source: 'im',
         layout: {
             visibility: 'visible',
         },
@@ -55,15 +70,15 @@ map.on('load', () => {
             'fill-extrusion-color': ['get', 'color'],
             'fill-extrusion-height': ['get', 'height'],
             'fill-extrusion-base': ['get', 'base_height'],
-            'fill-extrusion-opacity': .8
+            'fill-extrusion-opacity': 1
         },
         filter: ['==', ['get', 'level'], 1]
     });
 
     map.addLayer({
-        'id': 'room-extrusion2',
-        'type': 'fill-extrusion',
-        'source': 'im',
+        id: 'room-extrusion2',
+        type: 'fill-extrusion',
+        source: 'im',
         layout: {
             visibility: 'visible',
         },
@@ -71,7 +86,7 @@ map.on('load', () => {
             'fill-extrusion-color': ['get', 'color'],
             'fill-extrusion-height': ['get', 'height'],
             'fill-extrusion-base': ['get', 'base_height'],
-            'fill-extrusion-opacity': .9
+            'fill-extrusion-opacity': 1
         },
         filter: ['==', ['get', 'level'], 2]
     });
@@ -79,18 +94,28 @@ map.on('load', () => {
 
 const popup = new maplibregl.Popup({
     closeButton: true,
-    closeOnClick: true
+    closeOnClick: false
 });
 
 map.on('click', 'room-extrusion', (e) => {
     map.getCanvas().style.cursor = 'pointer';
 
-    const coordinates = e.features[0].geometry.coordinates.slice();
-    console.log(e.features[0].properties)
+    // console.log(e.features[0].properties)
 
     popup.setLngLat(e.lngLat).setHTML(`Комната: ${e.features[0].properties.name}<br>
     Площадь: ${e.features[0].properties.area_sqm}<br>
     <img class='photo-test' src='${e.features[0].properties.photo}'/>`).addTo(map)
 
 })
+
+map.on('click', 'room-extrusion2', (e) => {
+    map.getCanvas().style.cursor = 'pointer';
+
+    popup.setLngLat(e.lngLat).setHTML(`Комната: ${e.features[0].properties.name}<br>
+    Площадь: ${e.features[0].properties.area_sqm}<br>
+    <img class='photo-test' src='${e.features[0].properties.photo}'/>`).addTo(map)
+    
+
+})
+
 
